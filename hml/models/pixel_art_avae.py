ab8fa016164c94168f5bc8057e2912e7159fdb69
Hamish Morgan <ham430@gmail.com>
2022-04-27 11:14:41 +1000

    Start decayiong earlier

--- hml/models/pixel_art_avae.py
+++ hml/models/pixel_art_avae.py
@@ -1019,7 +1019,7 @@ def main(
     lr = LRS(
         max_lr=1e-4,
         min_lr=5e-6,
-        start_decay_epoch=100,
+        start_decay_epoch=30,
         stop_decay_epoch=800,
         steps_per_epoch=STEPS_PER_EPOCH,
     )
