469cedef6c93ec9fe9d776ac68bedada02a60ca7
Hamish Morgan <ham430@gmail.com>
2022-05-20 17:30:31 +1000

    Reduce weight on generated sharpness

--- hml/models/flood_sharpvae.py
+++ hml/models/flood_sharpvae.py
@@ -399,7 +399,7 @@ def compute_vae_loss(
     beta: float = 0e0,
     # gamma: float = 3e-1,
     gamma: float = 0e0,
-    delta: float = 1e-3,
+    delta: float = 1e-4,
     epsilon: float = 1e0,
     zeta: float = 1e0,
 ) -> Tuple[float, tf.Tensor, tf.Tensor, float, float, float]:
